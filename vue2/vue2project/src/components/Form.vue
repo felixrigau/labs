91529cc9b228af9f315cc02cbd7f0147a48ab33c
Felix Rigau <felixrigau88@gmail.com>
2018-04-17 22:46:51 +0000

    Form validations are ready

--- vue2/vue2project/src/components/Form.vue
+++ vue2/vue2project/src/components/Form.vue
@@ -1,16 +1,21 @@
 <template lang="pug">
-.form  
+form(@submit="validateForm" action="x" method="post")
+  p(v-if="errors.length")
+    b Please correct the following error(s):
+    ul
+      li(v-for="error in errors") {{ error }}
   input(v-model="course.title" placeholder="Título del Curso")
   input(v-model="course.subTitle" placeholder="Breve Description")
   input(v-model="course.startDate" type="date")
   input(v-model="course.endDate" type="date")
-  button(v-on:click="add") ADD
+  input(type="submit" value="Submit")
 </template>
 
 <script>
 export default {
   data() {
     return {
+      errors:[],
       course: {
         title: '',
         subTitle: '',
@@ -47,13 +52,33 @@ export default {
         course.finished = true;
       }
       return course;
+    },
+    validateForm: function(e) {
+      let now = Date.now(),
+          startDate = new Date(this.course.startDate),
+          endDate = new Date(this.course.endDate);
+      this.errors = [];
+      if(!this.course.title) this.errors.push("El título es obligatorio.");
+      if(!this.course.subTitle) this.errors.push("La descripción es obligatoria.");
+      if(!this.course.startDate) this.errors.push("La fecha de comienzo es obligatoria.");
+      if(!this.course.endDate) this.errors.push("La fecha de terminación es obligatoria.");
+      if(startDate.getTime() > endDate.getTime()) this.errors.push("Las fechas deben tener un orden lógico.");
+      if(startDate.getTime() === endDate.getTime()) this.errors.push("Las fechas no pueden ser iguales.");
+      
+      if(this.errors.length) {
+        e.preventDefault();
+      } else {
+        this.add(); 
+        e.preventDefault();
+      }
     }
   }
   /*
-  Para validar con las fechas:
-  startDate tiene que ser menos que endDate
-  endDate no puede ser meno que startDate
+  Para validar el formulario:
+  startDate tiene que ser menor que endDate
+  endDate no puede ser menor que startDate
   No pueden ser iguales
+  Los campos son obligatorios
   */
 }
 </script>
