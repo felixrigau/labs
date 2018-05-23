5c8720a304bc4c5b1259ef27a2af85e706f91293
felixrigau <felixrigau88@gmail.com>
2018-05-23 21:48:19 +0200

    refactoring locationList component

--- react-practices/weatherapp/src/App.js
+++ react-practices/weatherapp/src/App.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import LocationList from './components/LocationList';
 import './App.css';
 
+const cities = [
+  'La Habana',
+  'Lyon',
+  'Madrid',
+  'Paris',
+  'Málaga',
+  'Moscu'
+];
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LocationList/>
+        <LocationList cities = { cities }/>
       </div>
     );
   }
--- react-practices/weatherapp/src/components/LocationList.js
+++ react-practices/weatherapp/src/components/LocationList.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import WeatherLocation from './WeatherLocation';
 
 class LocationList extends Component {
+
+    constructor(cities) {
+        super();
+        
+        this.state = {
+            cities
+        }
+    }
+
+    arrToComponents = cities => (
+        cities.map( city => (<WeatherLocation city={ city }></WeatherLocation>))
+    );
+    
     render() {
+        const {cities} = this.state.cities;
+        debugger;
         return (
             <div>
-                <WeatherLocation city={'La Habana'}></WeatherLocation>
-                <WeatherLocation city={'Lyon'}></WeatherLocation>
-                <WeatherLocation city={'Madrid'}></WeatherLocation>
+                { this.arrToComponents( cities ) }
             </div>
         );
     }
+}
+
+LocationList.propTypes = {
+    cities: PropTypes.array.isRequired
 }
 
 export default LocationList;
