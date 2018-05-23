cd2d13338f19314f2bc39628a24ef6866a74bd83
felixrigau <felixrigau88@gmail.com>
2018-05-23 11:09:54 +0200

    Creating ClassComponent and configuring the state

--- react-practices/weatherapp/src/components/WeatherLocation/index.js
+++ react-practices/weatherapp/src/components/WeatherLocation/index.js
@@ -1,20 +1,49 @@
-import React, { component } from 'react';
+import React, { Component } from 'react';
 import Location from './Location'
 import WeatherData from './WeatherData';
-import { SNOW } from './../../constants/weathers';
+import { SUN, CLOUDY } from './../../constants/weathers';
 
-const data = {
+const data1 = {
+    temperature: 33,
+    weatherState: SUN,
+    humidity: 70,
+    wind: 10
+};
+
+const data2 = {
     temperature: 20,
-    weatherState: SNOW,
+    weatherState: CLOUDY,
     humidity: 40,
     wind: 10
 };
 
-const WeatherLocation = () => (
-    <div>
-        <Location city={'Buenos Aires!'}/>
-        <WeatherData data={ data } />
-    </div>
-);
+class WeatherLocation extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            city: 'La Habana',
+            data: data1
+        };
+    }
+
+    handleUpdateClick = () => {
+        this.setState({
+            city: 'Madrid',
+            data: data2
+        });
+    }
+
+    render = () => {
+        const { city, data } = this.state;
+        return (
+            <div>
+                <Location city={city}/>
+                <WeatherData data={data} />
+                <button onClick={this.handleUpdateClick} >Actualizar</button>
+            </div>
+        );
+    };
+}
 
 export default WeatherLocation; 
